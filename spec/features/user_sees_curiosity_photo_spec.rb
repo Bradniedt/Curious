7b7c2a0f75365e4983a0fd4ce27d34284467d3ea
Brad Niedt <bradleyniedt@gmail.com>
2019-02-20 19:29:09 -0700

    Adds change to feature test based on index page change

--- spec/features/user_sees_curiosity_photo_spec.rb
+++ spec/features/user_sees_curiosity_photo_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 describe 'As a user' do
   describe 'when I visit the root' do
     it 'should show me an image of space' do
-      visit '/'
+      VCR.use_cassette('feature') do
+        visit '/'
 
-      expect(page).to have_content("Welcome to Curious Space!")
-      expect(page).to have_content("Here's a photo from Mars:")
-      expect(page).to have_content("Sol (Martian Date):")
-      expect(page).to have_content("Rover:")
-      expect(page).to have_content("Camera:")
-      expect(page).to have_content("Landed on Mars:")
+        expect(page).to have_content("Welcome to Curious Space!")
+        expect(page).to have_content("Here's a photo of Mars:")
+        expect(page).to have_content("Sol (Martian Date):")
+        expect(page).to have_content("Rover:")
+        expect(page).to have_content("Camera:")
+        expect(page).to have_content("Landed on Mars:")
+      end
     end
   end
 end
